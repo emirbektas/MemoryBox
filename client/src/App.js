33fd5ccab74cb8701dc17eb7d2e7326ad53d6902
Emir <emirhan.bektas1@ogr.sakarya.edu.tr>
2022-02-27 12:33:12 +0300

    second commit

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,17 @@ import "./App.css";
 import HomeScreen from "./Screens/HomeScreen";
 import CreateScreen from "./Screens/CreateScreen";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import Header from "./components/Header";
+import { Container } from "react-bootstrap";
 
 function App() {
   return (
     <Router>
-      <Route path="/" component={HomeScreen} exact />
-      <Route path="/create" component={CreateScreen} />
+      <Header />
+      <Container>
+        <Route path="/" component={HomeScreen} exact />
+        <Route path="/create" component={CreateScreen} />
+      </Container>
     </Router>
   );
 }
